f8aff32a1c8f387c758c56e2432f27229d4f9f0c
Anthony Cavuoti <anthonycavuoti@gmail.com>
2022-12-01 19:13:28 -0800

    Add more to populate

--- project/325populate.sql
+++ project/325populate.sql
@@ -1,3 +1,41 @@
+DELETE FROM EXPERIENCE_ACCOMPLISHMENT;
+
+DELETE FROM EXPERIENCE;
+
+DELETE FROM CLASS_STAFF;
+
+DELETE FROM STUDENT_ACCOMPLISHMENTS;
+
+DELETE FROM ACCOMPLISHMENTS;
+
+DELETE FROM CLASS_STUDENTS;
+
+DELETE FROM PROJECT;
+
+DELETE FROM CLASS_SECTION;
+
+DELETE FROM COURSE_STUDENTS;
+
+DELETE FROM COURSE;
+
+DELETE FROM STUDENT_EMAIL;
+
+DELETE FROM STUDENT_PHONE;
+
+DELETE FROM STUDENT;
+
+DELETE FROM RESUME;
+
+DELETE FROM PORTFOLIO;
+
+DELETE FROM INSTRUCTOR_SUBJ_OF_EXPTSE;
+
+DELETE FROM INSTRUCTOR;
+
+DELETE FROM CAREER_COACH_INDUS_EXP;
+
+DELETE FROM CAREER_COACH;
+
 DELETE FROM MYNAN_STAFF;
 
 INSERT INTO MYNAN_STAFF VALUES(
@@ -53,4 +91,474 @@ INSERT INTO MYNAN_STAFF VALUES(
     '25-Jun-1994',
     1,
     0
+);
+
+INSERT INTO MYNAN_STAFF VALUES(
+    'P5i3D9P',
+    'Vera',
+    'Day',
+    'veraday@gmail.com',
+    0416370495,
+    '18-May-1994',
+    1,
+    0
+);
+
+INSERT INTO MYNAN_STAFF VALUES(
+    '4qE8K9C',
+    'Autumn',
+    'Mccoy',
+    'autumnmccoy@gmail.com',
+    0757770611,
+    '24-Nov-2008',
+    1,
+    0
+);
+
+INSERT INTO MYNAN_STAFF VALUES(
+    '1xJh34Y',
+    'Felix',
+    'Shannon',
+    'felixshannon@gmail.com',
+    1250788240,
+    '25-Feb-2018',
+    1,
+    0
+);
+
+INSERT INTO MYNAN_STAFF VALUES(
+    'Ewm821E',
+    'Hope',
+    'Thornton',
+    'thornton@gmail.com',
+    3593601781,
+    '14-May-2002',
+    1,
+    0
+);
+
+INSERT INTO MYNAN_STAFF VALUES(
+    'o9cY4b9',
+    'Zachary',
+    'Proctor',
+    'proctor@gmail.com',
+    5353956324,
+    '03-Nov-2001',
+    1,
+    0
+);
+
+INSERT INTO MYNAN_STAFF VALUES(
+    '4tJ02ih',
+    'Eleri',
+    'Walls',
+    'walls@gmail.com',
+    2166865910,
+    '20-May-2004',
+    0,
+    1
+);
+
+INSERT INTO MYNAN_STAFF VALUES(
+    'aAY423P',
+    'Nataniel',
+    'Meyer',
+    'meyer@gmail.com',
+    4545898813,
+    '06-Dec-1994',
+    0,
+    1
+);
+
+INSERT INTO MYNAN_STAFF VALUES(
+    '45Rz124',
+    'Nicole',
+    'Lara',
+    'lara@gmail.com',
+    3281104579,
+    '28-Oct-1991',
+    0,
+    1
+);
+
+INSERT INTO MYNAN_STAFF VALUES(
+    'pP9773z',
+    'Vivian',
+    'Sykes',
+    'sykes@gmail.com',
+    3895497823,
+    '08-May-2013',
+    0,
+    1
+);
+
+INSERT INTO MYNAN_STAFF VALUES(
+    'C887x04',
+    'Kristina',
+    'Harding',
+    'harding@gmail.com',
+    0900014204,
+    '11-Mar-2001',
+    0,
+    1
+);
+
+INSERT INTO MYNAN_STAFF VALUES(
+    '3mr9Z3X',
+    'Amelia',
+    'Villegas',
+    'villegas@gmail.com',
+    0911939526,
+    '14-Oct-1999',
+    0,
+    1
+);
+
+INSERT INTO MYNAN_STAFF VALUES(
+    '271O5Hk',
+    'Zahraa',
+    'Chandler',
+    'chandler@gmail.com',
+    7948835671,
+    '05-Jan-2023',
+    0,
+    1
+);
+
+INSERT INTO MYNAN_STAFF VALUES(
+    'o9e01Ta',
+    'Raheem',
+    'Gallagher',
+    'gallagher@gmail.com',
+    4228705708,
+    '08-Jun-2018',
+    0,
+    1
+);
+
+INSERT INTO MYNAN_STAFF VALUES(
+    'f9y4D52',
+    'Sumaiya',
+    'Osborn',
+    'osborn@gmail.com',
+    9778237163,
+    '03-Mar-2013',
+    0,
+    1
+);
+
+INSERT INTO MYNAN_STAFF VALUES(
+    'Gj9i72E',
+    'Tyrone',
+    'Hickman',
+    'hickman@gmail.com',
+    1652445468,
+    '06-Dec-1994',
+    0,
+    1
+);
+
+INSERT INTO CAREER_COACH VALUES(
+    'Qb5570t'
+);
+
+INSERT INTO CAREER_COACH VALUES(
+    'gmQ1L41'
+);
+
+INSERT INTO CAREER_COACH VALUES(
+    'P0uRG97'
+);
+
+INSERT INTO CAREER_COACH VALUES(
+    'fAf845g'
+);
+
+INSERT INTO CAREER_COACH VALUES(
+    '7Meq7g3'
+);
+
+INSERT INTO CAREER_COACH VALUES(
+    'P5i3D9P'
+);
+
+INSERT INTO CAREER_COACH VALUES(
+    '4qE8K9C'
+);
+
+INSERT INTO CAREER_COACH VALUES(
+    '1xJh34Y'
+);
+
+INSERT INTO CAREER_COACH VALUES(
+    'Ewm821E'
+);
+
+INSERT INTO CAREER_COACH VALUES(
+    'o9cY4b9'
+);
+
+INSERT INTO CAREER_COACH_INDUS_EXP VALUES(
+    'Qb5570t',
+    '3 year software analyst'
+);
+
+INSERT INTO CAREER_COACH_INDUS_EXP VALUES(
+    'gmQ1L41',
+    'quality assurance engineer for 2 years'
+);
+
+INSERT INTO CAREER_COACH_INDUS_EXP VALUES(
+    'P0uRG97',
+    'project manager for startup'
+);
+
+INSERT INTO CAREER_COACH_INDUS_EXP VALUES(
+    'fAf845g',
+    'senior product manager'
+);
+
+INSERT INTO CAREER_COACH_INDUS_EXP VALUES(
+    '7Meq7g3',
+    'business analyst for 5 years'
+);
+
+INSERT INTO CAREER_COACH_INDUS_EXP VALUES(
+    'P5i3D9P',
+    'web developer for 4 years'
+);
+
+INSERT INTO CAREER_COACH_INDUS_EXP VALUES(
+    '4qE8K9C',
+    'systems engineer for 2 years'
+);
+
+INSERT INTO CAREER_COACH_INDUS_EXP VALUES(
+    '1xJh34Y',
+    'technical recruiter for 1 year'
+);
+
+INSERT INTO CAREER_COACH_INDUS_EXP VALUES(
+    'Ewm821E',
+    'technical writer for 3 years'
+);
+
+INSERT INTO CAREER_COACH_INDUS_EXP VALUES(
+    'o9cY4b9',
+    'data analyst for 6 years'
+);
+
+INSERT INTO INSTRUCTOR VALUES(
+    '4tJ02ih',
+    'M W: 2:00pm-4:00pm and Tu, Th 1:00pm-2:30pm'
+);
+
+INSERT INTO INSTRUCTOR VALUES(
+    'aAY423P',
+    'M W: 5:00pm-6:30pm and Tu, Th 1:00pm-2:30pm'
+);
+
+INSERT INTO INSTRUCTOR VALUES(
+    '45Rz124',
+    'Tu, Th 3:00pm-4:00pm and F: 10:00am-12:00pm'
+);
+
+INSERT INTO INSTRUCTOR VALUES(
+    'pP9773z',
+    'M W: 8:00am-10:00am and Tu, Th 4:30pm-6:00pm'
+);
+
+INSERT INTO INSTRUCTOR VALUES(
+    'C887x04',
+    'M W: 11:00am-1:00pm and Tu, Th 3:00pm-4:30pm'
+);
+
+INSERT INTO INSTRUCTOR VALUES(
+    '3mr9Z3X',
+    'M W: 4:00pm-5:30pm and Tu, Th 10:00am-11:30am'
+);
+
+INSERT INTO INSTRUCTOR VALUES(
+    '271O5Hk',
+    'M W: 2:00pm-3:30pm and Tu, Th 9:00am-10:30am'
+);
+
+INSERT INTO INSTRUCTOR VALUES(
+    'o9e01Ta',
+    'M W: 6:00pm-7:30pm and Tu, Th 2:00pm-3:30pm'
+);
+
+INSERT INTO INSTRUCTOR VALUES(
+    'f9y4D52',
+    'M W: 9:00am-11:00am and Tu, Th 5:00pm-6:30pm'
+);
+
+INSERT INTO INSTRUCTOR VALUES(
+    'Gj9i72E',
+    'M W: 1:00pm-2:30pm and Tu, Th 8:00am-9:30am'
+);
+
+INSERT INTO INSTRUCTOR_SUBJ_OF_EXPTSE VALUES(
+    'aAY423P',
+    'Front-end Development'
+);
+
+INSERT INTO INSTRUCTOR_SUBJ_OF_EXPTSE VALUES(
+    '45Rz124',
+    'Back-end Development'
+);
+
+INSERT INTO INSTRUCTOR_SUBJ_OF_EXPTSE VALUES(
+    'pP9773z',
+    'Database Design and Administration'
+);
+
+INSERT INTO INSTRUCTOR_SUBJ_OF_EXPTSE VALUES(
+    'C887x04',
+    'UX/UI Design'
+);
+
+INSERT INTO INSTRUCTOR_SUBJ_OF_EXPTSE VALUES(
+    '3mr9Z3X',
+    'Content Management Systems (CMS)'
+);
+
+INSERT INTO INSTRUCTOR_SUBJ_OF_EXPTSE VALUES(
+    '271O5Hk',
+    'Responsive Design'
+);
+
+INSERT INTO INSTRUCTOR_SUBJ_OF_EXPTSE VALUES(
+    'o9e01Ta',
+    'E-commerce Development'
+);
+
+INSERT INTO INSTRUCTOR_SUBJ_OF_EXPTSE VALUES(
+    'o9e01Ta',
+    'Content Management Systems (CMS)'
+);
+
+INSERT INTO INSTRUCTOR_SUBJ_OF_EXPTSE VALUES(
+    'o9e01Ta',
+    'Search Engine Optimization (SEO)'
+);
+
+INSERT INTO INSTRUCTOR_SUBJ_OF_EXPTSE VALUES(
+    '3mr9Z3X',
+    'Web Hosting and Server Management'
+);
+
+INSERT INTO INSTRUCTOR_SUBJ_OF_EXPTSE VALUES(
+    '3mr9Z3X',
+    'Search Engine Optimization (SEO)'
+);
+
+INSERT INTO INSTRUCTOR_SUBJ_OF_EXPTSE VALUES(
+    'pP9773z',
+    'Back-end Development'
+);
+
+INSERT INTO INSTRUCTOR_SUBJ_OF_EXPTSE VALUES(
+    'pP9773z',
+    'Web Animation and Multimedia Integration'
+);
+
+INSERT INTO INSTRUCTOR_SUBJ_OF_EXPTSE VALUES(
+    'pP9773z',
+    'Custom Web Application Development'
+);
+
+INSERT INTO INSTRUCTOR_SUBJ_OF_EXPTSE VALUES(
+    'f9y4D52',
+    'Social Media Integration and Marketing'
+);
+
+INSERT INTO INSTRUCTOR_SUBJ_OF_EXPTSE VALUES(
+    'Gj9i72E',
+    'Website Analytics and Testing'
+);
+
+INSERT INTO INSTRUCTOR_SUBJ_OF_EXPTSE VALUES(
+    'Gj9i72E',
+    'Virtual Reality Development'
+);
+
+INSERT INTO INSTRUCTOR_SUBJ_OF_EXPTSE VALUES(
+    'f9y4D52',
+    'Front-end Development'
+);
+
+INSERT INTO INSTRUCTOR_SUBJ_OF_EXPTSE VALUES(
+    'f9y4D52',
+    'Artificial Intelligence'
+);
+
+INSERT INTO INSTRUCTOR_SUBJ_OF_EXPTSE VALUES(
+    '271O5Hk',
+    'Mobile App Development'
+);
+
+INSERT INTO INSTRUCTOR_SUBJ_OF_EXPTSE VALUES(
+    '271O5Hk',
+    'Front-end Development'
+);
+
+INSERT INTO PORTFOLIO VALUES(
+    'Gj9i72E',
+    'https://www.codejourney.net/1778',
+    'Qb5570t'
+);
+
+INSERT INTO PORTFOLIO VALUES(
+    '6PF3o9J',
+    'https://buildbetterwebsites.net/3pKG4y7',
+    'gmQ1L41'
+);
+
+INSERT INTO PORTFOLIO VALUES(
+    '9wMI09M',
+    'https://www.awesome-webdev.com/gY11Bm0',
+    'P0uRG97'
+);
+
+INSERT INTO PORTFOLIO VALUES(
+    '2x892PK',
+    'http://www.webdevportfolio.com/04WBhM1',
+    'fAf845g'
+);
+
+INSERT INTO PORTFOLIO VALUES(
+    'Fh53W7Q',
+    'http://www.webdev-solutions.net/k9I2R8G',
+    '7Meq7g3'
+);
+
+INSERT INTO PORTFOLIO VALUES(
+    '2K2s21N',
+    'http://www.webdesignerprofiles.com/SYr12m7',
+    'P5i3D9P'
+);
+
+INSERT INTO PORTFOLIO VALUES(
+    'Q7k08tT',
+    'https://www.webdevportfolio.org/s626Ulu',
+    '4qE8K9C'
+);
+
+INSERT INTO PORTFOLIO VALUES(
+    'lK0608u',
+    'https://www.webdev-showcase.com/s626Ulu',
+    '1xJh34Y'
+);
+
+INSERT INTO PORTFOLIO VALUES(
+    'uKCZ482',
+    'https://www.portfoliocreator.com/pzxjo',
+    'Ewm821E'
+);
+
+INSERT INTO PORTFOLIO VALUES(
+    'C221MBb',
+    'http://www.webdevportfolio.me/NEwm234',
+    'o9cY4b9'
 );
